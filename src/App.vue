63aca9949c51a71fc4f2948b597c21edd9aa0b34
张文皓 <zhangwh754@163.com>
2023-10-12 10:15:12 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+    <hr />
+  </header>
+
+  <RouterView />
+</template>
+
+<style lang="scss">
+nav {
+  a {
+    display: inline-block;
+    margin: 20px;
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="wrapper">
+    <h2>Home</h2>
+  </div>
+</template>
+
+<style scoped lang="scss">
+h2 {
+  color: red;
+}
+</style>
