2c8a9454816c281922784ee28c4857b19c58c140
张文皓 <zhangwh754@163.com>
2023-10-12 10:43:00 +0800

    设置vite base

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+export default defineConfig((mode) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: `/${env.VITE_APP_REPOSITORY_NAME}/`, // github仓库名称
+    plugins: [vue(), vueJsx()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  };
+});
